3c949206863f63af4341cf75f474cbf81d00dd29
Damir Cavar <dcavar@me.com>
2025-06-26 21:18:37 -0400

    updated tests

--- tests/test_nlqk.py
+++ tests/test_nlqk.py
@@ -12,7 +12,7 @@ Testing the NLQK corpus functionality.
 
 import sys
 sys.path.append('.')
-sys.path.append('..') # path to the module folder
+#sys.path.append('..') # path to the module folder
 import os
 import unittest
 from pathlib import Path
@@ -41,16 +41,13 @@ class NLQKTestLocal(unittest.TestCase):
 class NLQKTestEmbeddings(unittest.TestCase):
     """Testing the embeddings module."""
 
-    #@unittest.skipIf(os.environ.get("GITHUB_ACTIONS") == "true", "This test is deactivated on GitHub Actions")
     def test_is_normalized(self):
         """Test the is_normalized function."""
         from nlqk.embedding import is_normalized
         v = np.random.rand(8)
         v_norm = v / np.linalg.norm(v)
-        #print(embedding.is_normalized(v_norm))
         self.assertTrue(is_normalized(v_norm))
 
-    #@unittest.skipIf(os.environ.get("GITHUB_ACTIONS") == "true", "This test is deactivated on GitHub Actions")
     def test_normalize(self):
         """Test the normalize function."""
         from nlqk.embedding import normalize
--- tests/test_nlqk_corpus.py
+++ tests/test_nlqk_corpus.py
@@ -11,7 +11,7 @@ Testing the NLQK corpus functionality.
 
 import sys
 sys.path.append('.')
-sys.path.append('..') # path to the module folder
+#sys.path.append('..') # path to the module folder
 import os
 import unittest
 from pathlib import Path
